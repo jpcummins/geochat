cb37cea9be416a4e105204f8b9bb68be382f57d7
J.P. Cummins <jpcummins@gmail.com>
2015-04-20 10:40:23 -0700

    Added keepalive code for Heroku

--- app/controllers/zone.go
+++ app/controllers/zone.go
@@ -5,6 +5,7 @@ import (
 	"github.com/revel/revel"
 	"golang.org/x/net/websocket"
 	"github.com/TomiHiltunen/geohash-golang"
+	"time"
 )
 
 type Zone struct {
@@ -24,13 +25,11 @@ func (c Zone) Message(zone string, text string) revel.Result {
     }
 
 	z, ok := chat.FindZone(zone)
-
 	if !ok {
 		return nil
 	}
 
 	event, err := z.SendMessage(user, text)
-
 	if err != nil {
 		return c.RenderError(err)
 	}
@@ -51,19 +50,36 @@ func (c Zone) ZoneSocket(zone string, ws *websocket.Conn) revel.Result {
     s, z := chat.SubscribeToZone(zone, user)
     defer s.Unsubscribe()
 
+    // Send a heartbeat ping every 30 seconds to keep the Heroku connection alive
+	quit := make(chan struct{})
+	defer close(quit)
+	go func() {
+		ticker := time.NewTicker(30 * time.Second)
+	    for {
+	       select {
+	        case <- ticker.C:
+	            s.Events <- &chat.Event{Type: "ping", Data: nil, Timestamp: int(time.Now().Unix())}
+	        case <- quit:
+	            ticker.Stop()
+	            return
+	        }
+	    }
+	}()
+
     // Listen for client disconnects
     go func() {
         var msg string
         for {
             err := websocket.Message.Receive(ws, &msg)
             if err != nil {
+            	close(quit)
                 s.Unsubscribe()
                 return
             }
         }
     }()
 
-    // Send the archive 
+    // Send the archive
     if archive, err := z.GetArchive(10); err == nil {
         s.Events <- chat.NewEvent(archive)
     }
@@ -75,6 +91,7 @@ func (c Zone) ZoneSocket(zone string, ws *websocket.Conn) revel.Result {
 				// They disconnected.
                 leave := &chat.Leave{User: user}
                 z.Publish(chat.NewEvent(leave))
+                close(quit)
 				return nil
 			}
 		}
