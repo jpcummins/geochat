ad48b4318f3d56b2f840f5aea20df169c0452a27
J.P. Cummins <jpcummins@gmail.com>
2015-08-25 13:50:36 -0700

    Switched to HTTPS

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -536,7 +536,7 @@ var ZonePage = React.createClass({displayName: "ZonePage",
   },
 
   componentDidMount: function () {
-    this.socket = new WebSocket('ws://' + window.location.host + '/chat/socket')
+    this.socket = new WebSocket('wss://' + window.location.host + '/chat/socket')
     this.socket.onopen = this.wsOpened;
     this.socket.onclose = this.wsClosed;
     this.socket.onmessage = this.wsMessage;
